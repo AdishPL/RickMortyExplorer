8cd3939d2cf234f31feb1f872a968b7424af843c
Adrian Kaczmarek <adishpl@gmail.com>
2024-01-05 12:41:34 +0100

    Bootstrap project

---
+++ RickMortyExplorer/RickMortyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyExplorerApp.swift
+//  RickMortyExplorer
+//
+//  Created by Adrian Kaczmarek on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
